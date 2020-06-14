87e696ad03a86de5f961c9c21327ba7ba227258b
danieldeng2 <danieldeng223@gmail.com>
2020-06-14 20:28:44 +0100

    Better documentation for the core module, as well as the project.

--- src/core/include/tetris.h
+++ src/core/include/tetris.h
@@ -8,4 +8,11 @@
 
 #define FRAME_RATE 60
 typedef struct timespec timespec_t;
-void updateFrame(timespec_t *now, timespec_t *lastFrame, unsigned long *frameNum);
+
+/*
+ * waits until the timing is right, then advances the current frame by 1. User
+ * input is detected on every frame, print_state happens every frame, but the
+ * piece only drops on specific frames.  
+ */
+void updateFrame(timespec_t *now, timespec_t *lastFrame,
+                 unsigned long *frameNum);
