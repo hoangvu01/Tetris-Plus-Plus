9e247d2110cd36e17e6f79a72d258b2ded14f20f
zhao-xuan <1418756682@qq.com>
2020-06-14 16:28:51 +0800

    finished utilities for the genetic ai

--- src/genetic-ai/include/utils.h
+++ src/genetic-ai/include/utils.h
@@ -46,8 +46,10 @@ void free_param_array(param_state_t **param_array);
 /*
  * @brief: sort a list of parameter vectors by fitness
  * @param: param_state_t **param_array: an array of parameter vectors which will be sorted by fitness
+ * @param: int l: the left boundary for merge sort
+ * @param: int r: the right boundary for merge sort
  */
-void sort_param_array(param_state_t **param_array);
+void sort_param_array(param_state_t **param_array, int l, int r);
 
 /*
  * @brief: compute and set the fitness value to the given parameter vector
@@ -55,7 +57,7 @@ void sort_param_array(param_state_t **param_array);
  * @param: int iterations: specify how many simulations/rounds of games needed in order to compute the loss function for the given parameter vector
  * @param: int maxPieces: maximum number of pieces needed for each simulation/round of game
  */
-void computeLoss(param_state_t *param, int iterations, int maxPieces);
+void compute_loss(param_state_t *param, int iterations, int max_pieces);
 
 /*
  * @brief: randomly select certain percent of the parameter vectors in the param_array and return only the fittest ones among those
@@ -64,11 +66,11 @@ void computeLoss(param_state_t *param, int iterations, int maxPieces);
  * @param: int num: decide how many fittest ones to return by their loss value
  * @return: return the pointer to a array of length num which contain the fittest individual parameter vectors
  */
-param_state_t **selectFittest(param_state_t **param_array, double percentage, int num);
+param_state_t **select_fittest(param_state_t **param_array, int array_size, double percentage, int num);
 
 /*
  * @brief: generate the next generation of parameter vector using cross-over and insert them into the parameter vector array
  * @param: param_state_t **prev_param_array: fittest parameter vectors from previous generation
  * @param: param_state_t **param_array: the array where the fittest individuals will be inserted, sorted by the loss value
  */
-param_state_t *generateChild(param_state_t **prev_param_array, param_state_t **param_array);
+void generate_child(param_state_t **prev_param_array, param_state_t **param_array, int array_size);
--- src/genetic-ai/utils.c
+++ src/genetic-ai/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "state.h"
 
 void normalize(param_state_t *param) {
   register double hw = param->aggregate_height_w;
@@ -60,24 +61,136 @@ void free_param_array(param_state_t **param_array) {
   }
   free(param_array);
 }
+static void merge(param_state_t param_array[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    param_state_t L[n1], R[n2];
+    for (int i = 0; i < n1; i++) L[i] = param_array[l + i];
+    for (int j = 0; j < n2; j++) R[j] = param_array[m + j + 1];
 
-void sort_param_array(param_state_t **param_array) {
-  int i = 0;
-  for (int i = 0; param_array[i] != NULL; i++) {
-    for (int j = 0; param_array[j] != NULL; j++) {
-      /* sort the param_array */
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i]->loss <= R[j]->loss) {
+            param_array[k] = L[i];
+            i++;
+        } else {
+            param_array[k] = R[j];
+            j++
+        }
+        k++;
     }
+
+    while (i < n1) {
+        param_array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        param_array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void sort_param_array(param_state_t **param_array, int l, int r) {
+  if (l < r) {
+      int m = l + (r - l) / 2;
+      mergeSort(param_array, l, m);
+      mergeSort(param_array, m + 1, r);
+      merge(param_array, l, m, r);
   }
 }
 
-void computeLoss(param_state_t *param, int iterations, int maxPieces) {
-  //TODO
+static double best_move(state_t *state, param_state_t *param, tetriminos_t *best_block, int index, int total) {
+    double best_loss = 0;
+    spawnTetriminos(state);
+    /* try different rotation/direction */
+    for (int i = 0; i < 4; i++) {
+        tetriminos_t *active_block = state->block;
+        state->rotation = clockwise(active_block, state->rotation);
+        state->pos.x = 0;
+        while (canMove(state)) {
+            while(dropPiece(state));
+            double curr_loss = 0;
+            if (index == (total - 1)) {
+                curr_loss = -param->aggregate_height_w*get_aggregate_height(state)
+                        + param->complete_line_w*get_complete_line(state)
+                        - param->hole_number_w*get_hole_number(state)
+                        - param->bumpiness_w*get_bumpiness(state);
+            } else {
+                curr_loss = best_move(state, param, best, index + 1, total);
+            }
+
+            if (curr_loss > best_loss) {
+                best_loss = curr_loss;
+                state->block->num_spin = state->rotation;
+                *best_tetriminos = state->block;
+            }
+
+            state->pos.x++;
+        }
+    }
+
+    return best_loss;
 }
 
-param_state_t **selectFittest(param_state_t **param_array, double percentage, int num) {
-  //TODO
+void compute_loss(param_state_t *param, int iterations, int max_pieces) {
+    double param_loss = 0;
+    for (int i = 0; i < iterations; i++) {
+        state_t state = initState();
+        double loss = 0;
+        double num_pieces = 0;
+        while (num_pieces++ < max_pieces && canMove(state)) {
+            tetriminos_t *best_block = NULL;
+            best_move(state, param, best_block, 0, 2);
+            while (dropPiece(state));
+            /* Must separate clearLines and dropPiece */
+            loss += clearLines(state->grid);
+        }
+        param_loss += loss;
+    }
+    param->loss = param_loss;
 }
 
-param_state_t *generateChild(param_state_t **prev_param_array, param_state_t **param_array) {
-  
+param_state_t **select_fittest(param_state_t **param_array, int array_size, double percentage, int num) {
+    int sample_size = (int) array_size * percentage;
+    param_state_t **sample = calloc(sample_size, sizeof(param_state_t *));
+    for (int i = 0; i < sample_size; i++) {
+        sample[i] = param_array[randomInteger(0, sample_size - 1)];
+    }
+    sort_param_array(sample, 0, sample_size);
+    sample = realloc(sample, (num + 1) * sizeof(param_state_t *));
+    sample[num] = NULL;
+
+    return sample;
+}
+
+void generate_child(param_state_t **prev_param_array, param_state_t **param_array, int array_size) {
+    param_state_t *param = malloc(sizeof(param_state_t));
+    param->aggregate_height_w = 0;
+    param->complete_line_w = 0;
+    param->hole_number_w = 0;
+    param->bumpiness_w = 0;
+    for (int i = 0; prev_param_array[i] != NULL; i++) {
+        param_state_t temp = prev_param_array[i];
+        param->aggregate_height_w += temp->aggregate_height_w * temp->loss;
+        param->complete_line_w += temp->complete_line_w * temp->loss;
+        param->hole_number_w += temp->hole_number_w * temp->loss;
+        param->bumpiness_w += temp->bumpiness_w * temp->loss;
+    }
+    normalize(param);
+    if (randomInteger(0, 100) < 5) {
+        if (randomInteger(0, 100) < 25)
+            param->aggregate_height_w += randomDouble(-0.2, 0.2);
+        else if (randomInteger(0, 100) < 25)
+            param->complete_line_w += randomDouble(-0.2, 0.2);
+        else if (randomInteger(0, 100) < 25)
+            param->hole_number_w += randomDouble(-0.2, 0.2);
+        else
+            param->bumpiness_w += randomDouble(-0.2, 0.2);
+    }
+    normalize(param);
+    param_array[array_size - 1] = param;
+    sort_param_array(param_array, 0, array_size);
 }
