a6df3335b891df6928f3ff85e12bcc80b64e567c
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-06-14 22:27:39 +0100

    Added file for qlearning

---
+++ src/rl-ai/include/qlearn.h
@@ -0,0 +1,12 @@
+
+
+#ifndef Q_LEARN_H
+#define Q_LEARN_H
+
+#include "game.h"
+#include "state.h"
+#include "grid.h"
+
+void play(state_t *);
+
+#endif
--- src/rl-ai/train.c
+++ src/rl-ai/train.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdboool.h>
+#include <stdbool.h>
 
 
 
