33e3555e51615a770fb06cd69ddfe8ab25713826
danieldeng2 <danieldeng223@gmail.com>
2020-06-16 14:59:05 +0100

    Added block synchronisation.

--- src/core/tetris.c
+++ src/core/tetris.c
@@ -1,11 +1,9 @@
-#include <time.h>
 
 #include "display.h"
 #include "game.h"
 
 int main(int argc, char const *argv[]) {
   int levelNum = startScreen();
-  srand(time(0));
 
   startGame(levelNum);
   return EXIT_SUCCESS;
