a18092f7b7c8dc220385ebb3cd61c03435287fac
Kaiyan Fan <kf619@ic.ac.uk>
2020-06-17 12:17:06 +0000

    Implements multi-threading for supporting two LED arrays

--- src/pi/gpio_led_array.c
+++ src/pi/gpio_led_array.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 /* Clock speed for SPI channels in Hz */
 #define SPI_SPEED 500000
@@ -23,14 +25,27 @@ static uint8_t data[4] = {0x0, 0x0, 0x0, 0x0};
 static uint8_t heart[8] = {0x00, 0x66, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C, 0x18};
 
 
-void display_led(void) {
+void *display_led_up(void *dummy) {
   while (1) {
     for (int j = 0; j < 8; j++) {
       data[0] = ~heart[j];
       data[1] = 0xFF;
       data[2] = 0xFF;
       data[3] = 0x01 << j;
-      wiringPiSPIDataRW(0, data, sizeof(data));
+      wiringPiSPIDataRW(UPPER_DISPLAY, data, sizeof(data));
+      delay(OUT_DELAY);
+    }
+  }
+}
+
+void *display_led_low(void *dummy) {
+  while (1) {
+    for (int j = 0; j < 8; j++) {
+      data[0] = heart[j];
+      data[1] = 0xFF;
+      data[2] = 0xFF;
+      data[3] = 0x01 << j;
+      wiringPiSPIDataRW(LOWER_DISPLAY, data, sizeof(data));
       delay(OUT_DELAY);
     }
   }
@@ -41,11 +56,19 @@ void init_led_arr(void) {
     fprintf(stderr, "Unable to initialize WiringPI\n");
     exit(EXIT_FAILURE);
   }
-  wiringPiSetup();
   wiringPiSPISetup(UPPER_DISPLAY, SPI_SPEED);
-  display_led();
+  wiringPiSPISetup(LOWER_DISPLAY, SPI_SPEED);
+
+  /* Creates a separate thread for the display */
+  pthread_t thread_id_u, thread_id_l;
+  pthread_create(&thread_id_u, NULL, display_led_up, NULL);
+  pthread_create(&thread_id_l, NULL, display_led_low, NULL);
 }
 
 int main(void) {
   init_led_arr();
+  printf("This keeps running\n");
+  sleep(5);
+  heart[0] = 0xFF;
+  sleep(5);
 }
